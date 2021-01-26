344dd596f30340f949910f1417abf094c7baf969
Javier Marcet <javier@marcet.info>
2021-01-26 02:21:38 +0100

    updateguide & tv_grab_es_movistartv: Set cache to 0 isntead of deleting it
    Signed-off-by: Javier Marcet <javier@marcet.info>

--- updateguide.sh
+++ updateguide.sh
@@ -2,8 +2,6 @@
 
 /usr/bin/docker ps | grep -q movistar_u7d || exit
 
-rm -fr /opt/xmltv/.xmltv/cache
-/usr/bin/docker exec -it movistar_u7d /app/tv_grab_es_movistartv --m3u /home/MovistarTV.m3u
 /usr/bin/docker exec -it movistar_u7d /app/tv_grab_es_movistartv --tvheadend /home/MovistarTV.m3u --output /home/guide.xml
 
 curl http://10.137.70.3:8888/reload_epg
