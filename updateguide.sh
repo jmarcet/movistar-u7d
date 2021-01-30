345f131d95df914e5c3d72422dee9f063d054ce7
Javier Marcet <javier@marcet.info>
2021-01-30 15:44:26 +0100

    updateguide.sh: 'docker exec -it ' needs a controlling TTY, cannot be run from system scripts
    Signed-off-by: Javier Marcet <javier@marcet.info>

--- updateguide.sh
+++ updateguide.sh
@@ -2,5 +2,5 @@
 
 /usr/bin/docker ps | grep -q movistar_u7d || exit
 
-/usr/bin/docker exec -it movistar_u7d /app/tv_grab_es_movistartv --tvheadend /home/MovistarTV.m3u --output /home/guide.xml
-/usr/bin/docker exec -it movistar_u7d /usr/bin/wget -qO /dev/null http://127.0.0.1:8889/reload_epg
+/usr/bin/docker exec movistar_u7d /app/tv_grab_es_movistartv --tvheadend /home/MovistarTV.m3u --output /home/guide.xml
+/usr/bin/docker exec movistar_u7d /usr/bin/wget -qO /dev/null http://127.0.0.1:8889/reload_epg
