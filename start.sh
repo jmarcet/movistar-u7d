abcfdc3b7fb9294ba41d9438128d36ae7e3a1d68
Javier Marcet <javier@marcet.info>
2021-11-15 14:00:34 +0100

    start.sh: include tv_grab's log in container's logs, reset it on start
    Signed-off-by: Javier Marcet <javier@marcet.info>

--- start.sh
+++ start.sh
@@ -2,10 +2,13 @@
 
 [ -n "${U7D_UID}" ] && _SUDO="s6-setuidgid ${U7D_UID}"
 
+_tvgrab_log="${HOME}/.xmltv/tv_grab_es_movistartv.log"
+rm -f "${_tvgrab_log}"; ${_SUDO} touch "${_tvgrab_log}"
+
 ( while (true); do nc -z -v -w 1 172.26.23.3 53 2>/dev/null && break; sleep 1; done )
 ( while (true); do nice -n -10 ionice -c 2 -n 0 ${_SUDO} /app/movistar_epg.py; sleep 1; done ) &
 ( while (true); do nc -z -v -w 1 127.0.0.1 8889 2>/dev/null && break; sleep 1; done )
 ( while (true); do nice -n -15 ionice -c 1 -n 0 ${_SUDO} /app/movistar_u7d.py; sleep 1; done ) &
 
-tail -f /dev/null
+tail -f "${_tvgrab_log}"
 
