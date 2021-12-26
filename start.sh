196424e1706be25bbb39afd51afc7bb912fa5749
Javier Marcet <javier@marcet.info>
2021-12-26 14:28:06 +0100

    start.sh: fix permission issue on first run and make tail bulletproof, however slow the device might be
    Signed-off-by: Javier Marcet <javier@marcet.info>

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 [ -n "${U7D_UID}" ] && _SUDO="s6-setuidgid ${U7D_UID}"
-[ -e "${HOME}/.xmltv" ] || mkdir -p "${HOME}/.xmltv"
+[ -e "${HOME}/.xmltv" ] || ${_SUDO} mkdir -p "${HOME}/.xmltv"
 
 _tvgrab_log="${HOME}/.xmltv/tv_grab_es_movistartv.log"
-rm -f "${_tvgrab_log}"; ${_SUDO} touch "${_tvgrab_log}"
+${_SUDO} echo > "${_tvgrab_log}"
 
 ( while (true); do nc -z -v -w 1 172.26.23.3 53 2>/dev/null && break; sleep 1; done )
 ( while (true); do nice -n -10 ionice -c 2 -n 0 ${_SUDO} /app/movistar_epg.py; sleep 1; done ) &
 ( while (true); do nc -z -v -w 1 127.0.0.1 8889 2>/dev/null && break; sleep 1; done )
 ( while (true); do nice -n -15 ionice -c 1 -n 0 ${_SUDO} /app/movistar_u7d.py; sleep 1; done ) &
 
-tail -f "${_tvgrab_log}"
+${_SUDO} tail -F "${_tvgrab_log}"
 
