767c9451c4a28dcc05ec8ace6c103fb8d72a4833
Javier Marcet <javier@marcet.info>
2022-04-04 18:35:59 +0200

    Update UA used in health.sh, reading it from mu7d, and add it to docker-compose
    Signed-off-by: Javier Marcet <javier@marcet.info>

--- health.sh
+++ health.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-wget -U 'MICA-IP-STB' -qO- "http://www-60.svc.imagenio.telefonica.net:2001/appserver/mvtv.do?action=getCatchUpUrl&extInfoID=" | grep -q 'Missing TV program ID parameter'
+eval `grep 'UA =' mu7d.py | sed -e 's:^.\*UA = :export UA=:'`
+wget -U "${UA}" -qO- "http://www-60.svc.imagenio.telefonica.net:2001/appserver/mvtv.do?action=getCatchUpUrl&extInfoID=" | grep -q 'Missing TV program ID parameter'
