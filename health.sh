835e93984a2115197e7c8d4a0f8497711c1f698b
Javier Marcet <javier@marcet.info>
2022-06-11 13:03:05 +0200

    health.sh: fix error in health script
    Signed-off-by: Javier Marcet <javier@marcet.info>

--- health.sh
+++ health.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-eval `grep 'UA =' mu7d.py | sed -e 's:^.\*UA = :export UA=:'`
+eval `grep 'UA =' mu7d.py | sed -e 's:^.*UA = :export UA=:'`
 wget -U "${UA}" -qO- "http://www-60.svc.imagenio.telefonica.net:2001/appserver/mvtv.do?action=getCatchUpUrl&extInfoID=" | grep -q 'Missing TV program ID parameter'
